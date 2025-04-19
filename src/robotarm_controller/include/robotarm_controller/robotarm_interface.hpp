dee9a8f316cbe36cc2475b0f9839350656c9e13f
roach <giraftw2002@gmail.com>
2025-04-19 23:10:15 +0800

    AMEND: rename, beautify name space and prepare for hybird ack, h/w flow ctrl for h/w interace and arduino communicate in harmoic. ADD:parseFeeback method

--- src/robotarm_controller/include/robotarm_controller/robotarm_interface.hpp
+++ src/robotarm_controller/include/robotarm_controller/robotarm_interface.hpp
@@ -6,32 +6,35 @@
 #include <rclcpp_lifecycle/lifecycle_node.hpp>
 #include <rclcpp_lifecycle/state.hpp>
 #include <hardware_interface/system_interface.hpp>
-
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
 #include <vector>
 #include <string>
 #include <libserial/SerialPort.h>
 #include <libserial/SerialStream.h>
-namespace robotarm_controller
-{
-    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+namespace hw = hardware_interface;  // ✨ alias once
 
-    class RobotArmInterface : public hardware_interface::SystemInterface
+namespace robot_arm_ns
+{
+    class RobotArmInterface : public hw::SystemInterface
     {
     public:
         RobotArmInterface();
         virtual ~RobotArmInterface();
         // ROS 2 Lifecycle Methods
-        virtual CallbackReturn on_init(const hardware_interface::HardwareInfo &info) override;
-        virtual CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
-        virtual CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
+        virtual hw::CallbackReturn on_init(const hw::HardwareInfo &info) override;
+        virtual hw::CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
+        virtual hw::CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
+        virtual hw::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
 
         // Hardware Interface Methods
-        virtual std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-        virtual std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
-        hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;        
-        hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;        
+        virtual std::vector<hw::StateInterface> export_state_interfaces() override;
+        virtual std::vector<hw::CommandInterface> export_command_interfaces() override;
+        hw::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;        
+        hw::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;        
 
     private:
+        void parseFeedback(const std::string& msg);
+
         // Serial port for communication with the robot arm
         LibSerial::SerialPort arduino;
         std::string port_;
